784cd80a83f2ca2afe613707514563e24aaed78a
Kiopsau <28NickS2010@gmail.com>
2025-06-28 23:45:19 -0400

    added scroll to top button 6/26/25

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-function loadingerror(id, this) {
-    this.style.display = 'none'; 
-    document.getElementById(id).style.display='block'; 
+function loadingerror(fallbackid, imgid) {
+    document.getElementById(imgid).style.display = 'none'; 
+    document.getElementById(fallbackid).style.display='block'; 
+} 
+
+function scrollup() {
+    console.log("scrooll"); 
+    window.scrollTo({
+        top: 0, 
+        behavior: "smooth"
+    }); 
 }
