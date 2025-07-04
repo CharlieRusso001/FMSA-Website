daff19815d560ffb48d10f03bfd333144c643385
Kiopsau <28NickS2010@gmail.com>
2025-07-04 15:28:27 -0400

    Created Upcoming Events Home Page Section 7/4/25

--- script.js
+++ script.js
@@ -20,4 +20,25 @@ function navbarbuttonclick(id) {
     }
 
     document.getElementById(id).style.textDecoration = "underline"; 
+} 
+
+let homeupcomingtextstate = false; 
+function homeupcomingtext() {
+    if (homeupcomingtextstate == false) { //expand 
+        document.getElementById("homeupcomingdescription").innerHTML = "Created by our student-led music association, this international playlist blends live performances from young musicians with AI-generated pieces composed using student-written algorithms. Combining music, technology, and therapy, it’s designed to support emotional well-being in hospitals and retirement homes—helping seniors reconnect with memories and find comfort through sound. "; 
+        document.getElementById("homeupcomingh3").style.transform = "translateY(-75%)"; 
+        document.getElementById("homeupcomingdescription").style.transform = "translateY(-40%)"; 
+        document.getElementById("homeupcomingbutton").style.transform = "translate(36%, -600%)"; 
+        document.getElementById("homeupcomingbutton").innerHTML = "Show Less"; 
+    } else { //shrink 
+        document.getElementById("homeupcomingdescription").innerHTML = "Created by our student-led music association, this international playlist blends live performances from young musicians with AI-generated pieces composed using student-written algorithms. Combining music..."; 
+        document.getElementById("homeupcomingh3").style.transform = "none"; 
+        document.getElementById("homeupcomingdescription").style.transform = "none"; 
+        document.getElementById("homeupcomingbutton").style.transform = "none"; 
+        document.getElementById("homeupcomingbutton").style.transform = "translate(36%, 0%)"; 
+        document.getElementById("homeupcomingbutton").innerHTML = "Show More"; 
+    }
+    homeupcomingtextstate = !homeupcomingtextstate; 
+
+
 }
